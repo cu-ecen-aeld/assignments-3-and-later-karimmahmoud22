b266422364a49b103135931c622cf8fe0a35451d
karimmahmoud22 <karm.mohamed003@eng-st.cu.edu.eg>
2025-05-10 01:21:25 +0300

    Assignemtn #3 part #2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,11 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
+	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+	make -j$(nproc) ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+	make -j$(nproc) ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs    
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +53,9 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p rootfs
+cd rootfs
+mkdir -p bin dev etc home lib proc sbin sys tmp usr/bin usr/lib usr/sbin var lib64
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +64,51 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+	make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make -j$(nproc) ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp -a $SYSROOT/lib/ld-linux-aarch64.so.1 lib/
+cp -a $SYSROOT/lib64/libc.so.6 lib64/
+cp -a $SYSROOT/lib64/libm.so.6 lib64/
+cp -a $SYSROOT/lib64/libresolv.so.2 lib64/
 
 # TODO: Make device nodes
+echo "Creating device nodes"
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 622 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+echo "Building writer utility"
+cd $FINDER_APP_DIR
+make clean
+make CROSS_COMPILE=$CROSS_COMPILE
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo "Copying finder files to target rootfs"
+cp writer ${OUTDIR}/rootfs/home/
+cp finder.sh finder-test.sh autorun-qemu.sh ${OUTDIR}/rootfs/home/
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp conf/assignment.txt conf/username.txt ${OUTDIR}/rootfs/home/conf/
+sed -i 's|\.\./conf/assignment.txt|conf/assignment.txt|' ${OUTDIR}/rootfs/home/finder-test.sh
 
 # TODO: Chown the root directory
+echo "Setting ownership to root"
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+echo "Creating initramfs"
+find . | cpio -H newc -ov --owner root:root | gzip > ${OUTDIR}/initramfs.cpio.gz
